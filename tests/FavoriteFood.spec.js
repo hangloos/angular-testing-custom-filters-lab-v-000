59dd19173a2444e7bfc53e40587efb65a0af970f
Eric Loos <ericloos00@gmail.com>
2016-12-15 12:05:21 -0600

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,26 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		favoriteFood = $filter('favoriteFood')
 	}));
+
+  it ('should filter by the food enterted', function () {
+
+    var fakePeople = [{
+      name: "Eric",
+      favoriteFood: "Sandwiches"
+    }, {
+      name: "Molly",
+      favoriteFood: "Nachos"
+    }]
+
+    expect(favoriteFood(fakePeople,"Sandwiches").length).toBe(1)
+    expect(favoriteFood(fakePeople,"Sandwiches")[0].name).toEqual("Eric")
+
+  })
 
 	
 });
